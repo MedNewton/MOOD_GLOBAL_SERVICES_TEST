afbdb081122d960b23e0f69c5579e6d044499f1d
MedNewton <med.newton.2013@gmail.com>
2023-02-14 14:59:43 +0100

    Fixed the tooltip

--- src/components/sections/partials/enhancedBarsChart.js
+++ src/components/sections/partials/enhancedBarsChart.js
@@ -4,11 +4,12 @@ import { max } from "d3-array";
 
 
 const Tooltip = ({ x, y, data }) => {
+    console.log(data.x);
     return (
       <div style={{ position: "absolute", left: x, top: y }}>
           <div style={{ backgroundColor: "rgba(0,0,0,0.7)", color: "#fff", padding: "10px" }}>
-              <div>dd</div>
-              <div>df</div>
+              <div>{data.x}</div>
+              <div>{data.y + "M"}</div>
           </div>
       </div>
     );
